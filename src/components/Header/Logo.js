9c5c31e8a68de0da737ea87d02bc59e240ceb303
ttsoncs <tson25102002@gmail.com>
2024-04-13 21:39:01 +0700

    Refactor Logo component in Logo.js

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -4,38 +4,20 @@ import styled from "styled-components";
 
 function Logo({ setIsOpen }) {
   return (
-    <Wrapper>
-      <StyledLink
-        href="/"
-        onClick={() => setIsOpen(false)}
-      >
-        ttson.
-      </StyledLink>
-      <Badge>Portfolio</Badge>
-    </Wrapper>
+    <StyledLink
+      href="/"
+      onClick={() => setIsOpen(false)}
+    >
+      ttson.
+    </StyledLink>
   );
 }
-
-const Wrapper = styled.div`
-  display: flex;
-  gap: 2px;
-`;
 
 const StyledLink = styled(Link)`
   color: var(--color-primary-text);
   font-size: ${24 / 16}rem;
   font-weight: var(--font-weight-semibold);
   text-decoration: none;
-`;
-
-const Badge = styled.div`
-  align-self: flex-start;
-  background-color: var(--color-background);
-  border-radius: 6px;
-  border: 1px solid var(--color-border);
-  color: var(--color-primary-text);
-  font-size: ${12 / 16}rem;
-  padding: 2px 6px;
 `;
 
 export default Logo;
