1da1a1561d8d49edb8619a62ad6c83f310f5e107
ttsoncs <tson25102002@gmail.com>
2024-04-13 21:18:49 +0700

    Update Logo component in Logo.js

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -4,20 +4,37 @@ import styled from "styled-components";
 
 function Logo({ setIsOpen }) {
   return (
-    <Wrapper
-      href="/"
-      onClick={() => setIsOpen(false)}
-    >
-      ttson.
+    <Wrapper>
+      <StyledLink
+        href="/"
+        onClick={() => setIsOpen(false)}
+      >
+        ttson.
+      </StyledLink>
+      <Badge>Portfolio</Badge>
     </Wrapper>
   );
 }
 
-const Wrapper = styled(Link)`
+const Wrapper = styled.div`
+  display: flex;
+  gap: 2px;
+`;
+
+const StyledLink = styled(Link)`
   color: var(--color-primary-text);
   font-size: ${24 / 16}rem;
   font-weight: var(--font-weight-semibold);
   text-decoration: none;
+`;
+
+const Badge = styled.div`
+  color: var(--color-primary-text);
+  font-size: ${12 / 16}rem;
+  background-color: var(--color-border);
+  align-self: flex-start;
+  padding: 2px 6px;
+  border-radius: 4px;
 `;
 
 export default Logo;
