22d56c0d4ed8f5d46e5e8a79f4a2355f14a5b881
ttsoncs <tson25102002@gmail.com>
2024-04-15 12:12:11 +0700

    Add privacy policy page

---
+++ src/app/privacy/page.js
@@ -0,0 +1,319 @@
+"use client";
+
+import styled from "styled-components";
+import Link from "next/link";
+
+function Privacy() {
+  return (
+    <MaxWidthWrapper>
+        <Title>Privacy Policy</Title>
+        <Time>Last updated: April 10, 2024</Time>
+        <Text>
+          At Trinh The Son (hereinafter referred to as “I,” “me,” or “my”), I am
+          committed to protecting the privacy and security of your personal
+          information. This Privacy Policy outlines how I collect, use, and
+          disclose your personal information, as well as the types of personal
+          information that I may hold about you. It also explains how I
+          safeguard your personal information, the choices you have regarding
+          its use and disclosure, and how you can access and correct your
+          personal information. This Privacy Policy applies to all interactions
+          with me, including when you visit my portfolio website (the “Site”) at{" "}
+          <StyledLink href="/">ttson.vercel.app</StyledLink>.
+        </Text>
+        <Text>
+          By accessing or using the Site, you consent to the collection, use,
+          and disclosure of your personal information by me in accordance with
+          this Privacy Policy. If you withdraw your consent, I may not be able
+          to provide access to the Site. The terms and conditions stated in this
+          Privacy Policy apply to all visitors and others who access or use the
+          Site.
+        </Text>
+        <Text>
+          If you no longer wish for your personal information to be disclosed or
+          used for the purposes for which you originally consented, you can opt
+          out by notifying me through one of the methods provided at the end of
+          this Privacy Policy.
+        </Text>
+        <Text>
+          I will make reasonable efforts to inform third parties with whom I
+          have shared your personal information, as permitted under this Privacy
+          Policy, of your decision to opt out. If you choose not to opt out, I
+          may continue to collect, use, and disclose your personal information
+          as outlined in this Privacy Policy. However, please note that there
+          may be certain uses from which you cannot opt out, such as providing
+          services that you have requested from me.
+        </Text>
+        <Text>
+          I reserve the right to amend this Privacy Policy at my discretion,
+          without prior individual notice. The revised Privacy Policy will be
+          effective as of the date indicated at the top of this policy. It is
+          your responsibility to verify whether any amendments have been made,
+          and I encourage you to periodically check the date and review this
+          Privacy Policy for the most up-to-date information regarding my
+          privacy practices. If you object to any amendments, please discontinue
+          using the Site.
+        </Text>
+        <SubTitle>1. What Personal Information Do I Collect?</SubTitle>
+        <Text>
+          I collect personal information about you when you interact with me.
+          The specific types of information I may ask for depend on the nature
+          of your interaction with me. This information may include:
+        </Text>
+        <List>
+          <ListItem>
+            <strong>Communication information.</strong> I may collect the
+            information you provide when you communicate with me for support,
+            feedback, event participation, or any other interactions. This
+            includes the information shared when you contact me for assistance,
+            provide feedback on my work, participate in events I organize, or
+            engage with me in any other way.
+          </ListItem>
+          <ListItem>
+            <strong>Device information.</strong> I may collect information about
+            your browser and device, which can include your IP address, browser
+            type, operating system type, unique device identifier, and log
+            information. This information helps me analyze and improve the
+            performance and security of my services.
+          </ListItem>
+          <ListItem>
+            <strong>Analytics information.</strong> I may collect information
+            about your usage of my site using tracking snippets, such as Google
+            Analytics. These tools provide aggregated insights, including
+            information about which pages were viewed and for how long. These
+            snippets utilize cookies and other locally-stored technologies. The
+            information collected about your activity on my site includes pages
+            viewed and access time. I prioritize respecting user privacy by
+            enabling the most restrictive data collection options possible in
+            relation to the{" "}
+            <StyledLink href="https://fpf.org/thank-you-for-visiting-allaboutdnt-com/">
+              Do Not Track
+            </StyledLink>{" "}
+            setting.
+          </ListItem>
+          <ListItem>
+            <strong>Preferences information.</strong> I track certain
+            preferences, such as whether or not “Dark mode“ is enabled, using
+            cookies or local storage. This allows me to personalize your
+            experience on my site. You have the option to erase this data by
+            clearing site data through your browser{"'"}s developer tools.
+          </ListItem>
+        </List>
+        <SubTitle>2. Why do I collect your personal information?</SubTitle>
+        <Text>I use your personal information for the following purposes:</Text>
+        <List>
+          <ListItem>
+            To respond to support tickets and help troubleshoot problems.
+          </ListItem>
+          <ListItem>To protect you and me against error.</ListItem>
+          <ListItem>To assess and manage operational risks.</ListItem>
+          <ListItem>
+            To conduct research and analysis for the development of my products
+            and services.
+          </ListItem>
+          <ListItem>
+            To maintain, improve, and analyze my site and the products I offer.
+          </ListItem>
+          <ListItem>
+            To communicate with you about existing and new services, products,
+            and offers by email, as permitted under Vietnam{"'"}s Anti-Spam
+            Legislation (Decree 91).
+          </ListItem>
+          <ListItem>
+            To customize your experience on my site to your particular
+            preferences or interests.
+          </ListItem>
+        </List>
+        <SubTitle>3. Who do I share your personal information with?</SubTitle>
+        <Text>
+          For the purposes set out above, I may disclose your personal
+          information to:
+        </Text>
+        <List>
+          <ListItem>
+            Third-party service providers to process or handle personal
+            information on my behalf.
+          </ListItem>
+          <ListItem>
+            Government agencies and law enforcement or regulatory authorities
+            when required by law.
+          </ListItem>
+          <ListItem>My insurers and external legal advisors.</ListItem>
+        </List>
+        <Text>
+          I do not and will never sell your personal information to others.
+        </Text>
+        <SubTitle>4. Where do I store your personal information?</SubTitle>
+        <Text>
+          I may transfer personal information that we collect about you outside
+          of Vietnam, for the purposes set out above and in the Privacy Policy,
+          including for processing by third-party service providers in
+          connection with those purposes. Third-party service providers may
+          perform processing activities outside of Vietnam.
+        </Text>
+        <Text>
+          I and third-party service providers may be required to disclose your
+          personal information to the courts, government authorities, law
+          enforcement or regulatory authorities of the country where personal
+          information is being stored in response to a valid demand or request
+          in accordance with applicable laws.
+        </Text>
+        <Text>
+          I require all third-party service providers to implement and maintain
+          technical and organizational measures to protect your personal
+          information in a manner consistent with my Privacy Policy.
+        </Text>
+        <SubTitle>
+          5. How long will I use, disclose and retain your personal information?
+        </SubTitle>
+        <Text>
+          I will retain your personal information only for as long as necessary
+          to accomplish the identified purpose for which it was collected, or as
+          required by law. When your personal information is no longer needed,
+          or once I am no longer required to retain it, I will erase it.
+        </Text>
+        <SubTitle>6. How do I protect your personal information?</SubTitle>
+        <Text>
+          I employ reasonable technical, administrative, and physical safeguards
+          and security measures to protect your personal information from
+          unauthorized access, use, disclosure, copying, loss, misuse,
+          alteration, modification, or destruction.
+        </Text>
+        <SubTitle>
+          7. How can you access and update your personal information?
+        </SubTitle>
+        <Text>
+          Please communicate with me using the methods listed at the bottom of
+          this document. As an individual, you have the right to access and
+          correct the information I have about you after I have received your
+          written request and authenticated your identity. To process your
+          request, I may ask you to provide information to verify your identity
+          and confirm the scope of your request. Subject to legal restrictions
+          or specific rights of refusal, I will inform you of the existence,
+          use, and disclosure of personal information relating to you, and
+          provide you with access to that information.
+        </Text>
+        <Text>
+          If you identify any personal information that is out-of-date,
+          incorrect, or incomplete, please let me know. If we agree that the
+          information needs to be corrected, I will make the necessary changes
+          and make reasonable efforts to communicate these changes to other
+          parties with whom the information may have been shared, to the extent
+          feasible and as required by applicable laws and regulations.
+        </Text>
+        <Text>
+          There may be limits on your right to access your information, for
+          example, if the information is subject to legal privilege, contains
+          confidential commercial information, relates to an investigation of a
+          breach of an agreement or law, or contains information of other
+          individuals that cannot be separated.
+        </Text>
+        <SubTitle>8. Links to other websites</SubTitle>
+        <Text>
+          As described in the{" "}
+          <StyledLink href="/terms">Terms of Use</StyledLink>, the Sites may
+          contain links to other websites. This Privacy Policy applies only to
+          the personal information that I collect, use, and disclose. I am not
+          responsible or liable for the privacy practices of third parties, and
+          I strongly recommend that you review a third party{"'"}s privacy
+          policy before disclosing any personal information to them.
+        </Text>
+        <SubTitle>9. Children privacy</SubTitle>
+        <Text>
+          You must be at least 18 years of age or be of legal age of majority in
+          your jurisdiction to use or access the Sites.
+        </Text>
+        <Text>
+          I do not knowingly collect personal information directly from
+          children. If you are a parent or guardian and you are aware that your
+          child has provided me with personal information, please contact me. If
+          I become aware that I have collected personal information from
+          children, I will take reasonable steps to locate and remove that
+          information from my servers and records.
+        </Text>
+        <SubTitle>10. Changes to this policy</SubTitle>
+        <Text>
+          I may update Privacy Policy from time to time. I ask that you
+          periodically check the date and review this Privacy Policy for the
+          latest information on privacy practices. If I update my Privacy, I
+          will post the most recent version on the Sites.
+        </Text>
+        <SubTitle>11. Contacting me</SubTitle>
+        <Text>
+          If you have any questions, concerns, or requests relating to my
+          handling of your personal information, please direct them to the
+          contact information provided below. Please note that in order to
+          verify your identity and ensure the security of your personal
+          information, I may need to ask you for personal information to verify
+          your identity.
+        </Text>
+        <Text>
+          <strong>By email:</strong>{" "}
+          <StyledLink href="mailto:ttson.cs@gmail.com">
+            ttson.cs@gmail.com
+          </StyledLink>
+        </Text>
+        <Text>
+          <strong>By post:</strong>
+          <br />
+          Trinh The Son
+          <br />
+          212A3 Nguyen Trai #71000,
+          <br />
+          District 1, Ho Chi Minh City, Vietnam
+        </Text>
+    </MaxWidthWrapper>
+  );
+}
+
+const MaxWidthWrapper = styled.main`
+  margin-inline: auto;
+  max-width: 1100px;
+  padding-top: 80px;
+`;
+
+const Title = styled.h1`
+  color: var(--color-primary-text);
+  font-size: ${40 / 16}rem;
+  font-weight: var(--font-weight-bold);
+  text-align: center;
+`;
+
+const Time = styled.p`
+  color: var(--color-secondary-text);
+  font-size: ${16 / 16}rem;
+  font-style: italic;
+  margin-bottom: 64px;
+  text-align: center;
+`;
+
+const Text = styled.p`
+  color: var(--color-primary-text);
+  font-size: ${16 / 16}rem;
+  margin-bottom: 16px;
+`;
+
+const StyledLink = styled(Link)`
+  color: var(--color-primary);
+`;
+
+const SubTitle = styled.h2`
+  color: var(--color-tertiary);
+  font-size: ${24 / 16}rem;
+  font-weight: var(--font-weight-bold);
+  margin-bottom: 8px;
+  margin-top: 32px;
+`;
+
+const List = styled.ol`
+  list-style-type: upper-alpha;
+  margin-bottom: 32px;
+  padding-left: 18px;
+`;
+
+const ListItem = styled.li`
+  color: var(--color-primary-text);
+  font-size: ${16 / 16}rem;
+  margin-bottom: 16px;
+  padding-left: 16px;
+`;
+export default Privacy;
