be0633547a5b7822b15ab381f4462b983e61adac
ttsoncs <tson25102002@gmail.com>
2024-04-15 13:22:26 +0700

    Refactor Header component to include a background gradient and backdrop-filter

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,27 +19,29 @@ function Header() {
   // useResizeToggle(setIsOpen, 550);
 
   return (
-    <MaxWidthWrapper>
-      <Wrapper>
-        <Logo setIsOpen={setIsOpen} />
-        <Toggle
-          handelClick={handelClick}
-          isOpen={isOpen}
-          size={36}
-        />
-        <DesktopNav handelClick={handelClick} />
-        <MobileNav
-          isOpen={isOpen}
-          handelClick={handelClick}
-        />
-      </Wrapper>
-    </MaxWidthWrapper>
+    <>
+      <MaxWidthWrapper>
+        <Wrapper>
+          <Logo setIsOpen={setIsOpen} />
+          <Toggle
+            handelClick={handelClick}
+            isOpen={isOpen}
+            size={36}
+          />
+          <DesktopNav handelClick={handelClick} />
+          <MobileNav
+            isOpen={isOpen}
+            handelClick={handelClick}
+          />
+        </Wrapper>
+        <Background />
+      </MaxWidthWrapper>
+    </>
   );
 }
 
 const MaxWidthWrapper = styled.header`
   align-items: center;
-  background-color: var(--color-background);
   display: flex;
   height: 64px;
   justify-content: space-between;
@@ -58,6 +60,20 @@ const Wrapper = styled.div`
   @media ${QUERIES.phoneAndSmaller} {
     align-items: center;
   }
+`;
+
+const Background = styled.div`
+  background-image: radial-gradient(
+    transparent 1px,
+    var(--color-background) 1px
+  );
+  backdrop-filter: blur(4px);
+  background-size: 4px 4px;
+  height: 100%;
+  position: absolute;
+  top: 0px;
+  width: 100%;
+  z-index: -1;
 `;
 
 export default Header;
