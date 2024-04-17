0320e82b0c44386cba4763f0d5e70abd27843d7b
ttsoncs <tson25102002@gmail.com>
2024-04-17 21:28:11 +0700

    Refactor background styles, add pattern overlay, and update font size in Background.js

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -4,14 +4,37 @@ import React from "react";
 import styled from "styled-components";
 
 function Background({ children }) {
-  return <Wrapper>{children}</Wrapper>;
+  return (
+    <Wrapper>
+      <PatternWrapper>
+        <Pattern />
+      </PatternWrapper>
+      {children}
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.body`
   background-color: var(--color-background);
-  background-image: linear-gradient(to right, #80808012 1px, transparent 1px),
-    linear-gradient(to bottom, #80808012 1px, transparent 1px);
+`;
+
+const PatternWrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+`;
+
+const Pattern = styled.div`
+  background-image: linear-gradient(to right, #4f4f4f2e 1px, transparent 1px),
+    linear-gradient(to bottom, #4f4f4f2e 1px, transparent 1px);
   background-size: 24px 24px;
+  mask-image: radial-gradient(
+    ellipse 60% 50% at 50% 0%,
+    #000 70%,
+    transparent 100%
+  );
+  position: absolute;
+  inset: 0;
 `;
 
 export default Background;
