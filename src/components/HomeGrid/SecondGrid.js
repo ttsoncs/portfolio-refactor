d178b04b64fac5b3cc705203f6d2c5e78f4c4d4c
ttsoncs <tson25102002@gmail.com>
2024-04-18 15:04:35 +0700

    Remove unused image import and related code in SecondGrid.js

--- src/components/HomeGrid/SecondGrid.js
+++ src/components/HomeGrid/SecondGrid.js
@@ -1,14 +1,11 @@
 import { QUERIES } from "@/constants";
-import Image from "next/image";
 import React from "react";
 import styled from "styled-components";
 import TextLoop from "@/components/TextLoop";
-import megaphone from "/public/images/megaphone.webp";
 
 function SecondGrid() {
   return (
     <Wrapper>
-      <StyledImage src={megaphone} alt="Megaphone" />
       <TextLoop />
     </Wrapper>
   );
@@ -26,21 +23,6 @@ const Wrapper = styled.section`
     aspect-ratio: 16 / 5;
     grid-column: 1 / -1;
     grid-row: initial;
-  }
-`;
-
-const StyledImage = styled(Image)`
-  position: absolute;
-  height: 96px;
-  width: 96px;
-  top: 25%;
-  left: -5%;
-  transform: rotate(-10deg);
-
-  @media ${QUERIES.phoneAndSmaller} {
-    height: 72px;
-    width: 72px;
-    top: 25%;
   }
 `;
 
