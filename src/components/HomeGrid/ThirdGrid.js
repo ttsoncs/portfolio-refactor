99fe8fe59480801e4f99e005a76d6a362db4e3e4
ttsoncs <tson25102002@gmail.com>
2024-04-21 01:09:34 +0700

    Add image and text to ThirdGrid.js

--- src/components/HomeGrid/ThirdGrid.js
+++ src/components/HomeGrid/ThirdGrid.js
@@ -1,23 +1,29 @@
 import { QUERIES } from "@/constants";
+import box from "/public/images/package.svg";
+import Image from "next/image";
 import React from "react";
 import styled from "styled-components";
 
 function ThirdGrid() {
   return (
-    <Wrapper>
-      <Text>GitHub</Text>
-    </Wrapper>
+    <MaxWidthWrapper>
+      <Wrapper>
+        <StyledImage
+          src={box}
+          alt="Box"
+        />
+        <Text>GitHub</Text>
+      </Wrapper>
+    </MaxWidthWrapper>
   );
 }
 
-const Wrapper = styled.section`
+const MaxWidthWrapper = styled.section`
   background-color: var(--color-box-background);
   border-radius: 20px;
   border: 1px dashed var(--color-border);
-  display: grid;
   grid-column: 9/11;
   grid-row: 1/4;
-  place-content: center;
   padding: 28px;
 
   @media ${QUERIES.phoneAndSmaller} {
@@ -27,14 +33,31 @@ const Wrapper = styled.section`
   }
 `;
 
+const Wrapper = styled.div`
+  align-items: center;
+  aspect-ratio: 1/1;
+  display: grid;
+  grid-template-rows: repeat(3, 1fr);
+  height: 100%;
+  width: 100%;
+`;
+
 const Text = styled.p`
+  align-self: end;
   color: var(--color-primary-text);
-  font-size: ${48 / 16}rem;
+  font-size: ${20 / 16}rem;
   font-weight: var(--font-weight-medium);
+  grid-row: 3/-1;
 
   @media ${QUERIES.phoneAndSmaller} {
-    font-size: ${32 / 16}rem;
+    font-size: ${18 / 16}rem;
   }
+`;
+
+const StyledImage = styled(Image)`
+  grid-row: 1/3;
+  height: 100%;
+  width: 100%;
 `;
 
 export default ThirdGrid;
